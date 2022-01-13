c8d65c2a55f245c031980dafbd4182b8a2be6842
Kieran Emery <kizwiz@hotmail.co.uk>
2022-01-13 04:08:46 +0000

    KE-0000-Example: Looping through Arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -14,8 +14,22 @@ namespace Arrays
              * The below example will print out 'BMW' as it is the second string in the car array (as arrays start at 1).
              * Additionally, the numbers of values in the brackets (it’s currently 5) must match the array size, so if you added another car company, for example Honda, it would give a compile-time error. When you declare the array type like ‘string’ or ‘integer’, you cannot store a double or any other data type.*/
 
+            /*
             string[] car = new string[5] { "Audi", "BMW", "Ferrari", "Dodge", "Lexus" };
             Console.WriteLine(car [1]); Console.ReadLine();
+            */
+
+            /* Looping through arrays
+             * Using a foreach statement you can loop through an array and get all the values.*/
+
+            string[] cars = new string[] { "Audi", "BMW", "Toyota", "Honda", "Lexus" };
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+            Console.ReadLine();
+
         }
     }
 }
