b39d1194726ac46301ffa4e2059cbdca7d5ab251
Kieran Emery <kizwiz@hotmail.co.uk>
2022-01-13 04:05:40 +0000

    Ke-000-Example: Initial Build

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,12 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            /* We can identify an array by the square brackets []
+             * The below example will print out 'BMW' as it is the second string in the car array (as arrays start at 1).
+             * Additionally, the numbers of values in the brackets (it’s currently 5) must match the array size, so if you added another car company, for example Honda, it would give a compile-time error. When you declare the array type like ‘string’ or ‘integer’, you cannot store a double or any other data type.*/
+
+            string[] car = new string[5] { "Audi", "BMW", "Ferrari", "Dodge", "Lexus" };
+            Console.WriteLine(car [1]); Console.ReadLine();
         }
     }
 }
